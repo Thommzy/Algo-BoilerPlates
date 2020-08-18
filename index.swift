dcb4ded78458a1ab125f670dc5f87c4712c2bbaa
Thommzy <tomiwaobeisun@yahoo.com>
2020-08-18 14:56:14 +0100

    added raise Power Boilerplate

---
+++ index.swift
@@ -0,0 +1,16 @@
+//Raise Power
+/*
+func pow(_ x: Int, _ y: Int) -> Int {
+    var i = 0
+  
+    var sum: CLong = 1
+  
+    repeat{
+        let repeated = repeatElement(x, count: i).repeatedValue
+        sum *= repeated
+        i = i + 1
+    }while( i < y)
+  
+    return sum
+}
+*/
